f44548e06927bb345d359b5e4f550de393308d20
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-03-10 14:32:28 +0600

    Add rental associations

--- book.rb
+++ book.rb
@@ -1,8 +1,13 @@
 class Book
     attr_accessor :title, :author
-    
+
     def initialize(title, author)
         @title = title
         @author = author
+        @rentals = []
+    end
+
+    def add_rental(rental)
+        @rentals << rental
     end
 end
--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require './decorator'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age, name = 'unknown', parent_permission: true)
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -18,6 +19,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 
   private
